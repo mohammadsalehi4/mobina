7d7d6e4cce42683c3a800e6e94b69a93bed57014
Mohammad <mohammad7979salehi@gmail.com>
2023-11-05 17:39:57 +0330

    image

--- src/components/adminReports/adminReports.js
+++ src/components/adminReports/adminReports.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import DataTable from 'react-data-table-component'
-import { Card, CardHeader, CardTitle } from 'reactstrap'
+import { Card, CardHeader, CardTitle, UncontrolledTooltip } from 'reactstrap'
 
 const AdminReports = () => {
   const columns = [
@@ -13,7 +13,19 @@ const AdminReports = () => {
         if (row.status) {
           return (
             <>
-              <ion-icon style={{fontSize:'24px', color:'green'}} name="checkmark-outline"></ion-icon>
+              <ion-icon id={`reportStatus${row.id}`} style={{fontSize:'24px', color:'green'}} name="checkmark-outline"></ion-icon>
+              <UncontrolledTooltip placement='top' target={`reportStatus${row.id}`}>
+                منتشر شده
+              </UncontrolledTooltip>
+            </>
+          )
+        } else {
+          return (
+            <>
+              <ion-icon id={`reportStatus${row.id}`} style={{fontSize:'24px', color:'orange'}} name="arrow-undo-outline"></ion-icon>
+              <UncontrolledTooltip placement='top' target={`reportStatus${row.id}`}>
+                پیش‌نویس
+              </UncontrolledTooltip>
             </>
           )
         }
@@ -27,8 +39,8 @@ const AdminReports = () => {
     },
     {
       name: <p style={{marginTop:"15px", margin:"0px"}}>عنوان</p>,
-      minWidth: '300px',
-      maxWidth: '300px',
+      minWidth: '500px',
+      maxWidth: '500px',
       cell: row => (
         <p className='mt-3'>
           {row.title}
@@ -37,22 +49,30 @@ const AdminReports = () => {
     },
     {
       name: <p style={{marginTop:"15px", margin:"0px"}}>نویسنده</p>,
-      minWidth: '200px',
-      maxWidth: '200px',
-      cell: row => (<p>{row.name}</p>)
+      minWidth: '180px',
+      maxWidth: '180px',
+      selector: row => row.writer
     },
     {
       name: <p style={{marginTop:"15px", margin:"0px"}}>عملیات</p>,
       minWidth: '200px',
       maxWidth: '200px',
-      cell: row => (<p>{row.name}</p>)
+      cell: () => (
+        <div>
+          <ion-icon style={{fontSize:'24px'}} name="eye-outline"></ion-icon>
+          <ion-icon style={{fontSize:'24px'}} name="create-outline"></ion-icon>
+          <ion-icon style={{fontSize:'24px'}} name="trash-outline"></ion-icon>
+        </div>
+      )
     }
   ]
   const data = [
     {
-      status:true,
+      id:1,
+      status:false,
       date:'2023-06-14',
-      title:'مروری بر تاریخچه پروژه‌های کلاهبرداری رمز ارزها در ایران'
+      title:'مروری بر تاریخچه پروژه‌های کلاهبرداری رمز ارزها در ایران',
+      writer:'محمد صالحی'
     }
   ]
   return (
