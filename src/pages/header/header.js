f1892526999e542ae11f54c4d25423fa5e8f736a
Mohammad <mohammad7979salehi@gmail.com>
2023-12-03 15:28:00 +0330

    bug fix

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -89,7 +89,7 @@ function Header() {
 
             <div class="navbar-brand app-brand demo d-none d-xl-flex py-0"  style={{marginRight:"60px"}}>
               <a href="/" class="app-brand-link gap-2">
-                <img src='/images/logo.png' id='logo'/>
+                <img src='/images/logo22.png' id='logo'/>
                 <p className='vazir' id='brandName'>پنتا</p>
               </a>
             </div>
