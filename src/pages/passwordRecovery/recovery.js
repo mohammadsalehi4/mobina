d97b220f1e56c68c98a539ffeee31d96a675440c
Mohammad <mohammad7979salehi@gmail.com>
2024-01-01 15:23:01 +0330

    KPI 2

--- src/pages/passwordRecovery/recovery.js
+++ src/pages/passwordRecovery/recovery.js
@@ -111,7 +111,7 @@ const Recovery = () => {
                                         theme="dark"
                                         size="invisible"
                                         hl='fa'
-                                        sitekey={'6LeTDjcpAAAAAPUoTa4fbRcU6v_PrSBQFi0rQNuv'}
+                                        sitekey={'6LeMh0ApAAAAADLZc2OFx-POjZLkqtQmfIWvdJnj'}
                                         ref={recaptchaRef}
                                         onChange={recaptchaOnChange}
                                     />
