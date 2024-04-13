05db5b9febb8d7b255fc2d99f3d370bdb788a38c
Mohammad <mohammad7979salehi@gmail.com>
2024-04-13 17:44:51 +0330

    dashboard

--- src/pages/first_page/FirstPage.js
+++ src/pages/first_page/FirstPage.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Entities from './DashboardSections/Entities/Entities'
 import SlidBox from './DashboardSections/box/SlidBox'
 import RewardStats from './DashboardSections/RewardStats/RewardStats'
-import Chart from './DashboardSections/Charts/Chart'
+// import Chart from './DashboardSections/Charts/Chart'
 import Addresses from './DashboardSections/Addresses/Addresses'
 
 
